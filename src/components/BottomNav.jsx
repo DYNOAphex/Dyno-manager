f925d5222fb02175cc069a7cba4618db046c31b1
DYNOAphex <thibaut.llorens@hotmail.com>
2026-07-12 10:46:15 +0200

    Update BottomNav.jsx

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,40 +1,65 @@
 import { NavLink } from "react-router-dom";
 import {
-  FaHome,
-  FaCalendarAlt,
-  FaPlusCircle,
-  FaUsers,
-  FaCog,
-} from "react-icons/fa";
+  House,
+  CalendarDays,
+  Plus,
+  Users,
+  Shield,
+} from "lucide-react";
 
 import "./BottomNav.css";
 
 function BottomNav() {
   return (
     <nav className="bottom-nav">
-      <NavLink to="/" className="nav-item">
-        <FaHome />
+
+      <NavLink
+        to="/dashboard"
+        className={({ isActive }) =>
+          isActive ? "nav-item active" : "nav-item"
+        }
+      >
+        <House size={22} />
         <span>Accueil</span>
       </NavLink>
 
-      <NavLink to="/calendrier" className="nav-item">
-        <FaCalendarAlt />
+      <NavLink
+        to="/calendrier"
+        className={({ isActive }) =>
+          isActive ? "nav-item active" : "nav-item"
+        }
+      >
+        <CalendarDays size={22} />
         <span>Agenda</span>
       </NavLink>
 
-      <NavLink to="/ajouter" className="nav-item add-button">
-        <FaPlusCircle />
+      <NavLink
+        to="/create-scrim"
+        className="nav-add"
+      >
+        <Plus size={28} />
       </NavLink>
 
-      <NavLink to="/roster" className="nav-item">
-        <FaUsers />
-        <span>Équipe</span>
+      <NavLink
+        to="/roster"
+        className={({ isActive }) =>
+          isActive ? "nav-item active" : "nav-item"
+        }
+      >
+        <Users size={22} />
+        <span>Roster</span>
       </NavLink>
 
-      <NavLink to="/plus" className="nav-item">
-        <FaCog />
-        <span>Plus</span>
+      <NavLink
+        to="/admin"
+        className={({ isActive }) =>
+          isActive ? "nav-item active" : "nav-item"
+        }
+      >
+        <Shield size={22} />
+        <span>Admin</span>
       </NavLink>
+
     </nav>
   );
 }

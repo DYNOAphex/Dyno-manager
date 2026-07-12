ec21d515414ba371d68dcfc14918aa0854dc57ca
DYNOAphex <thibaut.llorens@hotmail.com>
2026-07-12 09:39:38 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,51 +7,43 @@ export default defineConfig({
     react(),
 
     VitePWA({
-
       registerType: "autoUpdate",
 
       includeAssets: [
-        "favicon.ico",
-        "logo192.png",
-        "logo512.png"
+        "favicon.svg",
+        "icon-192.png",
+        "icon-512.png"
       ],
 
       manifest: {
-
         name: "DYNO Manager",
         short_name: "DYNO",
-
         description: "Gestionnaire d'équipe Esport",
 
         theme_color: "#d4af37",
         background_color: "#111111",
 
         display: "standalone",
-
         orientation: "portrait",
-
         start_url: "/",
 
         icons: [
-
           {
-            src: "/icon-192.png",
+            src: "icon-192.png",
             sizes: "192x192",
             type: "image/png"
           },
-
           {
-            src: "/icon-512.png",
+            src: "icon-512.png",
             sizes: "512x512",
             type: "image/png"
           }
-
         ]
+      },
 
+      devOptions: {
+        enabled: true
       }
-
     })
-
   ]
-
 });
